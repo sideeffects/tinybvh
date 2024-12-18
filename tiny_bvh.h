e91fbe366bae0eb1d9e277b179cf0bbd45954cf9
Jacco Bikker <bikker.j@gmail.com>
2024-12-18 15:21:34 +0100

    Version bump before sync to main.

--- tiny_bvh.h
+++ tiny_bvh.h
@@ -116,7 +116,7 @@ THE SOFTWARE.
 // library version
 #define TINY_BVH_VERSION_MAJOR	1
 #define TINY_BVH_VERSION_MINOR	1
-#define TINY_BVH_VERSION_SUB	1
+#define TINY_BVH_VERSION_SUB	2
 
 // ============================================================================
 //
